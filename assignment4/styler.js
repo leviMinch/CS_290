6082fc429b2c522fb4d85331b1b792e166e54501
Levi Minch <minchle@oregonstate.edu>
2024-02-06 13:07:45 -0800

    more work, close to finished

--- assignment4/styler.js
+++ assignment4/styler.js
@@ -1,1 +1,18 @@
 "use strict";
+
+window.onload = function() {
+    // Add an event listener to the button A
+    document.getElementById('styleA').addEventListener('click', function(event) {
+        event.preventDefault();
+        console.log("ran A");
+        document.getElementById('styleSheet').setAttribute('href', "StyleA.css");
+    });
+
+    // Add an event listener to the button B
+    document.getElementById('styleB').addEventListener('click', function(event) {
+        event.preventDefault();
+        console.log("ran B");
+        document.getElementById('styleSheet').setAttribute('href', "StyleB.css");
+    });
+}
+
